1687fc490f3c349ec591f0fe07ee1b98eae7fd4b
Jackson Oppenheim <50721527+jacksonopp@users.noreply.github.com>
2022-08-10 17:54:41 -0500

    create get endpoints

---
+++ src/pages/api/user/username/[user].ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from '../../../../server/db/client';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.query);
+  const result = await prisma.contributor.count({
+    where: {
+      username: req.query.user as string,
+    }
+  })
+
+
+  console.log(result);
+
+  if (result === 0) {
+    return res.status(404).json({
+      message: "Not found",
+    });
+  }
+
+  res.status(302).send("ok");
+}
