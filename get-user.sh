112b6488fa96a7ad1f5d13d0f30ce68d48958bda
Jackson Oppenheim <50721527+jacksonopp@users.noreply.github.com>
2022-08-10 19:38:51 -0500

    create get user script

---
+++ get-user.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# get the user as the first argument
+USERNAME=$1
+
+echo "searching for user: $USERNAME"
+
+# save a curl request to the server with the username as the last part of the url
+res=$(curl -s "http://localhost:3000/api/user/username/$USERNAME")
+
+echo "result from server: $res"
+
+# if res is ok, exit with 0, if res is not found, exit with 1, if any other error, exit with 2
+if [ "$res" == "ok" ]; then
+    echo "found user"
+    exit 0
+elif [ "$res" == "not found" ]; then
+    echo "user not found"
+    exit 1
+else
+    echo "server error: please open an issue on github"
+    exit 2
+fi
